2ba43d708c8709205228bf808e87f1ea0bbb8bfe
joseMac <josefort@2tor.com.mx>
2019-05-15 00:19:03 -0500

    sql tables

--- sqlSchema.sql
+++ sqlSchema.sql
@@ -1,24 +1,24 @@
 use example;
-drop table skills;
+drop table if exists skills;
 create table skills(
     skill varchar(50) not null
 );
-drop table employees;
+drop table if exists employees;
 create table employees(
     email varchar(50) not null unique,
     password varchar(50) not null
 );
-#drop table subscriptions;
+drop table if exists subscriptions;
 create table subscriptions(
     email varchar(50) not null unique
 );
-#drop table employees_skills;
+drop table if exists employees_skills;
 create table employees_skills(
     skill varchar(50) not null,
     email_employee int unsigned not null 
 );
-#drop table subscriptions_skills;
+drop table if exists subscriptions_skills;
 create table subscriptions_skills(
-    varchar(50) not null,
-    email_subscription varchar(50) not null unique,
+    skill varchar(50) not null,
+    email_subscription varchar(50) not null unique
 );
